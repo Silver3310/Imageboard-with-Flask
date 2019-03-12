c2337dd3735f8e7a9fcff8a85da2de457497bbd6
Silver3310 <silver3310@github.com>
2019-03-12 16:43:15 +0700

    Add user authentication

--- gallery/config.py
+++
@@ -1,4 +0,0 @@
-# we're gonna keep global variables that we want for the application use in here
-class Config(object):
-    SQLALCHEMY_DATABASE_URI = 'postgres://sctqiewhhnvmer:74536945114a692e0b2c696d4824b5500cc9e1d8b0fdfaa08b3b746a6e8484e1@ec2-54-221-236-144.compute-1.amazonaws.com:5432/d2qcqv9deiss32'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
--- gallery/models.py
+++ gallery/models.py
@@ -1,6 +1,43 @@
 from gallery import db
 
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+
 db.create_all()
+
+
+class LoginUser(db.Model, UserMixin):
+    __tablename__ = 'user'
+
+    email = db.Column(
+        db.String,
+        primary_key=True
+    )
+    authenticated = db.Column(
+        db.Boolean,
+        default=False
+    )
+    pw_hash = db.Column(
+        db.String
+    )
+
+    def is_active(self):
+        return True
+
+    def get_id(self):
+        return self.email
+
+    def is_authenticated(self):
+        return self.authenticated
+
+    def is_anonymous(self):
+        return False
+
+    def set_password(self, password):
+        self.pw_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.pw_hash, password)
 
 
 class Appuser(db.Model):
@@ -46,4 +83,3 @@ class Appimage(db.Model):
     
     def __repr__(self):
         return f'<Appimage {self.URL}: {self.appuser.email}>'
-
